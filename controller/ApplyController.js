7901743e16038e3acb484b9b2fd89cc681d7b0b9
ConfidenceEfem <confidenceefem1@gmail.com>
2022-07-09 06:01:07 +0100

    apply error fixed

--- controller/ApplyController.js
+++ controller/ApplyController.js
@@ -43,22 +43,20 @@ const ApplyForAJob = async (req, res) => {
       if (!findJobs) {
         res.status(400).json({ message: "You don't have right for this" });
       }
-      // const image = await cloudinary.uploader.upload(req.file.path);
+      const image = await cloudinary.uploader.upload(req.file.path);
       const applyItem = new ApplyModel({
         email,
         name,
-        // cvImage: image.secure_url,
-        // cvImageid: image.public_id,
+        cvImage: image.secure_url,
+        cvImageid: image.public_id,
         applicationLetter,
       });
 
       applyItem.user = findUser;
+      applyItem.job = findJobs;
       applyItem.save();
       // applyItem.client = findClient;
       // applyItem.save();
-
-      applyItem.job = findJobs;
-      applyItem.save();
 
       findJobs.apply.push(mongoose.Types.ObjectId(applyItem._id));
       findJobs.save();
